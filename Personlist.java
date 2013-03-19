739c4593e0af0f057f92a12af02ed4862bea6152
Erik Haider Forsen <erik@forsen.no>
2013-03-19 14:21:53 +0100

    pseudokode

---
+++ Personlist.java
@@ -0,0 +1,15 @@
+public class Personlist
+{
+	<datafelter>
+
+	<konstruktør som oppretter listen>
+
+	<metode for å legge personer i listen>
+
+	<metode for å fjerne personer fra listen>
+
+	<metode for å søke personer basert på heiskort>
+
+	<metode for å søke personer på navn>
+
+}
