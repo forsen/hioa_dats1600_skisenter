64f2bf6ab29d517dd74671a2a2282daa6d90efea
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-03-19 14:23:34 +0100

    Pseudokode for klassens metoder, datafelt og konstruktør

---
+++ Skicardlist.java
@@ -0,0 +1,15 @@
+//a list over all of the skicards a person holds and all cards ever sold
+public class Skicardlist 
+{
+	<datafelt>
+
+	<konstruktør som oppretter startverdier for datafeltene>
+
+	<metode for innsetting av kort i listen>
+	<metode for fjerning av kort i listen>
+	<metode for finne/søke etter kort i listen etter kort nm)
+	<metode for å finne eier av kort>
+	<metode for å skjekke om listen er tom>
+
+
+}//end of class Skicardlist
