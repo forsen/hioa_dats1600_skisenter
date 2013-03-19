f3f41f5eb23351db36aa060a3b71206a60d984ac
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-03-19 14:14:13 +0100

    LITT pseudokode lagt til

---
+++ Skisenter.java
@@ -0,0 +1,18 @@
+public class Skisenter
+{
+	private Personlist personregistry;
+	private Liftlist liftregistry;
+	private Skicardlist cardregistry;
+
+	public Skisenter(Personlist p, Liftlist l, Skicardlist c)
+	{
+		personregistry = p;
+		liftregistry = l;
+		cardregistry = c;
+	}
+
+	
+
+
+
+}
