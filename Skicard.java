1bc788af46f796bc24bd7128f1fe74ba910c76c2
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-03-19 14:30:24 +0100

    pseudokode lagt til for klassen

---
+++ Skicard.java
@@ -0,0 +1,12 @@
+//Superclass for all the different cardtypes 
+
+public abstract class Skicard 
+{
+	<datafelt>
+
+	<konstuktør>
+
+	<astrakt get-metoder for å hente ut datafeltene til kortene>
+	<abstrakt metode for å skjekke om kortet er gyldig>
+	 
+}
