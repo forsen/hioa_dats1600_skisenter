5ac282b8ab9d94daa27a07136fbc4761204f8b6f
Erik Haider Forsen <erik@forsen.no>
2013-03-19 14:24:43 +0100

    pseudokode

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+	<datafelter, inkludert heiskortliste>
+
+	<konstruktør som oppretter personobjektet>
+
+	<metode for å returnere navn>
+
+	<metode for å returnere kundenr>
+
+	<metode for å registrere heiskort til navn>
+
+	
+}
