c92d55b9c74762207fb10f3b9d4e1059854f8dcb
Erik Haider Forsen <erik@forsen.no>
2013-03-19 13:35:40 +0100

    added pseudo code

---
+++ Hourcard.java
@@ -0,0 +1,22 @@
+public class Hourcard extends Skicard
+{
+	private boolean gyldig; 
+	private int heiskortnr; 
+
+	public Hourcard( <dataverdi?> )
+	{
+		gyldig = true; 
+	}
+
+	public boolean gyldig()
+	{
+		return gyldig;
+	}
+
+	public int getHeiskort()
+	{
+		return heiskortnr; 
+	}
+
+
+}
