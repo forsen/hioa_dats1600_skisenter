1c452df855b65d6bc79a0707a054f2ab5454f5e5
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-03-19 11:25:56 +0100

    Testa litt da vett !

--- Tullball.java
+++ Tullball.java
@@ -1,7 +1,10 @@
+import javax.swing.JOptionPane;
+
 public class Tullball
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Test");
+		JOptionPane.showMessageDialog(null,"Detta prosjektet kommer til å bli helt awesome !!");
 	}
 }
