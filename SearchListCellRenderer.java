2892e51470587fa6af451f4c65cdcd1f548a8a5d
Erik Forsén <erik@forsen.no>
2013-04-04 10:42:42 +0200

    .

--- SearchListCellRenderer.java
+++ SearchListCellRenderer.java
@@ -3,24 +3,15 @@ import java.awt.*;
 
 public class SearchListCellRenderer extends DefaultListCellRenderer
 {
-    public Component getListCellRendererComponent(
-        JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
-    {
-        // I know DefaultListCellRenderer always returns a JLabel
-        // super setups up all the defaults
-        JLabel label = (JLabel)super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+	public Component getListCellRendererComponent(
+	JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+	{
+		JLabel label = (JLabel)super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
-        // "value" is whatever object you put into the list, you can use it however you want here
+		Person p = (Person) value; 
 
-        Person p = (Person) value; 
+		label.setText( " " +  p.getLastName() + ", " + p.getFirstName() + " " );
 
-        // I'm going to prefix the label text to demonstrate the point
-
-       label.setText( " " +  p.getLastName() + ", " + p.getFirstName() + " " );
-
-       return label;
-
-    }
+		return label;
+	}
 }
-
-// Some time later...
