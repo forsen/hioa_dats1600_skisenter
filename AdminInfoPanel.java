dd9ac1d8cce668f9cd8a6eca0fcaf3195130c0c9
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-04-12 22:46:02 +0200

    la til nye klasser

---
+++ AdminInfoPanel.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*;
+
+public class AdminInfoPanel extends JPanel
+{	
+	private JButton showPersons,showPersWcards, showCards;
+	private JTextArea display;
+	private JPanel butnPnl, dispPnl;
+	private Listener listener;
+	private JScrollPane scroll;
+	private Personlist list;
+
+	public AdminInfoPanel(Personlist l)
+	{
+		list = l;
+		butnPnl = new JPanel(new GridLayout( 4,2 ));
+		dispPnl = new JPanel();
+		
+		setLayout( new BorderLayout( 5, 5) );
+
+		listener = new Listener();
+
+		display = new JTextArea(20,40);
+		scroll = new JScrollPane(display);
+
+		showPersons = new JButton(" Vis Person ");
+		showPersons.addActionListener( listener );
+		butnPnl.add(showPersons);
+
+		showCards = new JButton(" Vis kort ");
+		showCards.addActionListener( listener );
+		butnPnl.add(showCards);
+
+		showPersWcards = new JButton(" Vis personer med kort ");
+		showPersWcards.addActionListener( listener );
+		butnPnl.add(showPersWcards);
+		
+
+		dispPnl.add(display);
+
+		add(butnPnl);
+		add(dispPnl);
+
+
+
+	}
+
+	private class Listener implements ActionListener
+  	{
+   		public void actionPerformed( ActionEvent e )
+    	{ 	
+      		
+      		if(e.getSource() == showPersons)
+      		{
+      			
+      		}
+
+      		if(e.getSource() == showPersWcards)
+      		{
+      			
+      		}
+
+      		if(e.getSource() == showCards)
+      		{
+      			
+      		}
+      		
+    	}
+	}	
+}
+	
