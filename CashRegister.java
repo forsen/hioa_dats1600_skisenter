83ef176e57f5b44efe8dbe82a40f764d97963feb
Erik Haider Forsen <erik@forsen.no>
2013-04-13 13:16:34 +0200

    fikser på cashregister

--- CashRegister.java
+++ CashRegister.java
@@ -5,11 +5,14 @@ import java.awt.event.*;
 
 public class CashRegister extends JFrame
 {
-	private JTextArea orderList; 
+	private JTextArea orderList, total, overview; 
 	private ShoppingCart shoppingCart; 
 	private JButton printReceipt, printCard, payByCash, payByCard;
 	private JTextField cashInput;
 	private BtnListener btnListener; 
+	private JLabel cashInn;
+
+
 	
 	public CashRegister( ShoppingCart s )
 	{
@@ -18,26 +21,96 @@ public class CashRegister extends JFrame
 		shoppingCart = s;
 		btnListener = new BtnListener();
 
-		orderList = new JTextArea( 30, 20 );
+		orderList = new JTextArea(  );
+		orderList.setText( shoppingCart.toString() );
+
+		total = new JTextArea();
+		total.setText("Total: \t\t" + shoppingCart.getSum() + ",-");
+		overview = new JTextArea();
+		overview.setText(" overview ");
 		printReceipt = new JButton( "Print kvittering" );
 		printCard = new JButton( "Print skikort" );
 		payByCard = new JButton( "Betal med kort" );
 		payByCash = new JButton( "Betal med kontanter" );
 		payByCash.addActionListener( btnListener );
 		cashInput = new JTextField( 7 );
+		cashInput.setText("cashinput");
+		cashInn = new JLabel( "Betalt: " );
+
+		setLayout( new GridBagLayout() );
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(10,10,10,10);
+		c.gridheight = 4; 
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 0;
+		c.gridwidth = 2; 
+		c.weighty = 0.5;
+		add( orderList , c);
 
 
-		setLayout( new FlowLayout() );
+		c.gridheight = 1;
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 4; 
+		c.gridwidth = 2;
+		c.weighty = 0.5;
+		add( total, c );
+//		add( orderList );
 
-		add( orderList );
-		add( cashInput );
-		add( payByCash );
-		add( payByCard );
-		add( printCard );
-		add( printReceipt );
+		c.gridheight = 1;
+		c.gridx = 2;
+		c.gridy = 0;
+		c.gridwidth = 1;
 
-		printReceipt.setVisible( false );
-		printCard.setVisible( false );
+		add( cashInn, c );
+
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.gridx = 2;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		add( payByCard, c );
+
+		c.fill = GridBagConstraints.BOTH;
+		c.gridheight = 2;
+		c.gridx = 2;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		add( overview, c );
+
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.gridx = 2;
+		c.gridy = 4; 
+		c.gridwidth = 1;
+		add( printCard, c );
+
+		c.gridheight = 1;
+		c.gridx = 3;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		add( cashInput, c );
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.gridx = 3;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		add( payByCash, c );
+
+		c.gridheight = 1;
+		c.gridx = 3;
+		c.gridy = 4;
+		c.gridwidth = 1;
+		add( printReceipt, c );
+
+		pack();
+
+		
 		setSize( 400,400 );
 		setVisible( true ); 
 	}
