8aad60b3e6a7a8167cceed20b818d5ca9efc97a1
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-04-13 21:17:38 +0200

    addet calculator.java

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.util.*;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Iterator;
+
+public class Calculator
+{
+	private Personlist custRegistry;
+	//private List<Skicard> cardlist;
+	private List<Validations> validations;
+
+	public Calculator(Personlist cr, List<Validations> v )
+	{
+		custRegistry = cr;
+		//cardlist = cl;
+		validations = v;
+	}
+
+	public int totalRegPepole()
+	{
+		return custRegistry.totalRegPepole();
+	}
+
+	public int regThatTime(int nr)
+	{
+		return custRegistry.regThatTime(nr);
+	}
+
+	public int passesbyTypeofCard(int liftnr)
+	{
+		Iterator<Validations> it = validations.iterator();
+
+		int antall = 0;
+
+		while(it.hasNext())
+		{
+			if(it.next().getLiftId() == liftnr)
+				antall++;
+		}
+		return antall;
+	}
+
+}
