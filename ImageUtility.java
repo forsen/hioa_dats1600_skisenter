47a9e241cf65793832210c9c2d4280d9aa449100
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-04-14 03:32:49 +0200

    hjelpeklasse for bilder

---
+++ ImageUtility.java
@@ -0,0 +1,21 @@
+public class ImageUtility
+{	
+	static public void drawImage()
+	{
+		
+	}
+
+	static public void copyImage()
+	{
+
+	}
+
+	static public void saveImage()
+	{
+		
+	}
+
+	
+} 
+
+ //  klasse med statiske metoder for tegning og kopiering av bilder,
