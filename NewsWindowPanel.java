880ef74ab9e18c6bfec3ba5bcb4c393b9880baa9
Ole Hansen <olehansen92@gmail.com>
2013-04-30 16:08:45 +0200

    nå skal vi få til ordentlige saker her ja

--- NewsWindowPanel.java
+++ NewsWindowPanel.java
@@ -7,12 +7,16 @@ import java.util.Date;
 
 public class NewsWindowPanel extends JPanel
 {
-	private JLabel header, news1, news2, news3, news4;
+	private JLabel header, news1, news2, news3, news4, icon;
 
 
 
 	public NewsWindowPanel()
 	{
+
+		ImageIcon npic1 = new ImageIcon("img/npic1.png");
+		icon = new JLabel();
+		icon.setIcon(npic1);
 	
 
 		setLayout(new GridBagLayout() );
@@ -22,18 +26,25 @@ public class NewsWindowPanel extends JPanel
 		header = new JLabel("Sesongåpning 20. desember 2013!");
 		header.setFont(new Font("Calibri", Font.BOLD, 20));
 
-		news1 = new JLabel("<html>" +
-	    "Vi åpner skianlegget 20. desember i år. Kom for gratis kaffe og vafler.<br>" +
-	    "________________________________________________________________________________________________________ <br><br></head>");
+
+
+		news1 = new JLabel(
+	    "<html>Vi åpner skianlegget 20. desember i år. Kom for gratis kaffe og vafler.<br>" +
+	    "________________________________________________________________________________________________________ <br><br></html>");
 
 	    news1.setFont(new Font("Calibri", Font.PLAIN, 14));
 
 		c.gridx = 0;
 		c.gridy = 0;
-		c.anchor = GridBagConstraints.PAGE_START;
+		c.weighty=0.2;
+		c.fill = GridBagConstraints.VERTICAL;
+		c.anchor = GridBagConstraints.FIRST_LINE_START;
 		add(header, c);
 
-		c.gridy= 1;
+		c.gridy=1;
+		add(icon,c);
+
+		c.gridy= 2;
 		add(news1,c);
 	}
 
