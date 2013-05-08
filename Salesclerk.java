541ac9ed7d803b41ba08600167e747a2dbcc38e4
Ole Hansen <olehansen92@gmail.com>
2013-05-08 13:15:57 +0200

    fikset størrelse på knapper, samt rammer, og skrivefeil

--- Salesclerk.java
+++ Salesclerk.java
@@ -118,7 +118,7 @@ public class Salesclerk extends JFrame
 
 		ImageIcon next = new ImageIcon("img/neste.png");
 		nextCustBtn = new JButton(next);
-		nextCustBtn.setToolTipText("For å ekspidere neste kunde");
+		nextCustBtn.setToolTipText("For å ekspedere neste kunde");
 		ImageIcon next2 = new ImageIcon("img/neste2.png");
 		nextCustBtn.setRolloverIcon(next2);
 
