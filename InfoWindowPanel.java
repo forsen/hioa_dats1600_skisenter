c9c2ee5d9cd12aa34b3b4dbbe5b3e793ac31cecd
Ole Hansen <olehansen92@gmail.com>
2013-05-09 16:45:13 +0200

    litt endringer

--- InfoWindowPanel.java
+++ InfoWindowPanel.java
@@ -8,7 +8,7 @@ import javax.imageio.*;
 
 public class InfoWindowPanel extends JPanel
 {
-	private JLabel headerImg;
+	private JLabel headerImg1, headerImg2;
 	private JLabel header;
 	private JLabel content;
 
@@ -23,16 +23,46 @@ public class InfoWindowPanel extends JPanel
 		setBackground(new Color(220, 240, 255));
 
 		ImageIcon snowboarder = new ImageIcon("img/snowboarder.png");
+		ImageIcon skimap = new ImageIcon("img/skimap.png");
 
 
-		headerImg = new JLabel(snowboarder);
-		add(headerImg, c);
+
+
+
+//****** NYTT ELEMENT *************************************//
+		header = new JLabel("Skitrekket");
+		header.setFont(new Font("Calibri", Font.BOLD, 20));
+
+		c.gridx =0;
+		c.gridy =0;
+
+		add(header, c);
+
+		c.gridy=1;
+		headerImg2 = new JLabel(skimap);
+		add(headerImg2, c);
+
+		content = new JLabel("<html>Skitrekket består av to heiser, en stolheis og en krokheis. Vi har alt fra grønne til svarte løyper, <br>" +
+			"altså løyper for både nybegynnere og viderekomne.<br><br><br></html>");
+		content.setFont(new Font("Calibri", Font.PLAIN, 14));
+		content.setHorizontalAlignment(JLabel.CENTER);
+
+		c.gridy=2;
+		add(content, c);
+
+
+//****** NYTT ELEMENT *************************************//
+
+		headerImg1 = new JLabel(snowboarder);
+		
+		c.gridy=3;
+		add(headerImg1, c);
 
 
 		header = new JLabel("Åpningstider");
 		header.setFont(new Font("Calibri", Font.BOLD, 20));
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 4;
 		add(header,c);
 
 		content = new JLabel("<html>Mandag: 10:00-16:30<br>"
@@ -46,7 +76,7 @@ public class InfoWindowPanel extends JPanel
 	    content.setHorizontalAlignment(JLabel.CENTER);
 
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = 5;
 		c.anchor = GridBagConstraints.PAGE_START;
 		add(content, c);
 	}
--- NewsWindowPanel.java
+++ NewsWindowPanel.java
@@ -7,45 +7,96 @@ import java.util.Date;
 
 public class NewsWindowPanel extends JPanel
 {
-	private JLabel header, news1, news2, news3, news4, icon;
+	private JLabel header, news1, news2, news3, news4, icon1, icon2, icon3, icon4;
 
 
 
 	public NewsWindowPanel()
 	{
 
-		ImageIcon npic1 = new ImageIcon("img/npic1.png");
-		icon = new JLabel();
-		icon.setIcon(npic1);
-	
-
 		setLayout(new GridBagLayout() );
 		GridBagConstraints c = new GridBagConstraints();
 		setBackground(new Color(220, 240, 255));
-
-		header = new JLabel("Sesongåpning 20. desember 2013!");
-		header.setFont(new Font("Calibri", Font.BOLD, 20));
-
-
-
-		news1 = new JLabel(
-	    "<html>Vi åpner skianlegget 20. desember i år. Kom for gratis kaffe og vafler.<br>" +
-	    "________________________________________________________________________________________________________ <br><br></html>");
-
-	    news1.setFont(new Font("Calibri", Font.PLAIN, 14));
 
 		c.gridx = 0;
 		c.gridy = 0;
 		c.weighty=0.2;
 		c.fill = GridBagConstraints.VERTICAL;
 		c.anchor = GridBagConstraints.FIRST_LINE_START;
-		add(header, c);
 
-		c.gridy=1;
-		add(icon,c);
 
-		c.gridy= 2;
+		/***** 1. NYHET ******************************************************/
+		ImageIcon npic2 = new ImageIcon("img/npic2.png");
+		
+		icon1 = new JLabel();
+		icon1.setIcon(npic2);
+		add(icon1,c);
+
+		news1 = new JLabel(
+		"<html>Husk å melde på dine små troll og nisser til neste skiskole! Skiskolen starter 22. februar " +
+	    "og varer til 26. februar. <br> Ring oss for påmelding og mer informasjon!<br><br><br><br></html>");    
+
+	    news1.setFont(new Font("Calibri", Font.PLAIN, 14));
+		
+
+		c.gridy= 1;
 		add(news1,c);
+
+
+		/***** 2. NYHET ******************************************************/
+		ImageIcon npic1 = new ImageIcon("img/npic1.png");
+		icon2 = new JLabel();
+	    icon2.setIcon(npic1);
+
+		c.gridy=3;
+		add(icon2,c);
+
+		news2 = new JLabel(
+		"<html>Vi åpner skianlegget 20. desember i år. Kom for gratis kaffe og vafler.<br><br><br><br></html>");
+
+
+	    news2.setFont(new Font("Calibri", Font.PLAIN, 14));
+
+		c.gridy=4;
+		add(news2,c);
+
+		/***** 3. NYHET ******************************************************/
+
+		ImageIcon npic3 = new ImageIcon("img/npic3.png");
+		icon3 = new JLabel();
+	    icon3.setIcon(npic3);
+
+		c.gridy=5;
+		add(icon3,c);
+
+		news3 = new JLabel(
+		"<html>Vi åpner for mer kjøring i påsken. Mer kjøring, mer moro. Heisene vil være åpne for kveldskjøring hele påsken<br>" +
+		" mellom klokka 17 og 21. <br><br><br><br></html>");
+
+
+	    news3.setFont(new Font("Calibri", Font.PLAIN, 14));
+
+		c.gridy=6;
+		add(news3,c);
+
+		/***** 4. NYHET ******************************************************/
+
+		ImageIcon npic4 = new ImageIcon("img/npic3.png");
+		icon4 = new JLabel();
+	    icon4.setIcon(npic4);
+
+		c.gridy=7;
+		add(icon4,c);
+
+		news4 = new JLabel(
+		"<html>Vi åpner for mer kjøring i påsken. Mer kjøring, mer moro. Heisene vil være åpne for kveldskjøring hele påsken<br>" +
+		" mellom klokka 17 og 21. <br><br><br><br></html>");
+
+
+	    news4.setFont(new Font("Calibri", Font.PLAIN, 14));
+
+		c.gridy=8;
+		add(news4,c);
 	}
 
 
