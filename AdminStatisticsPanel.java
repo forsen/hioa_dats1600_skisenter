68189ce791a119d6e24f7e3a1651e63e7fc775c8
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-05-12 16:51:05 +0200

    endret navn daa

---
+++ AdminStatisticsPanel.java
@@ -0,0 +1,474 @@
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*;
+import java.util.List;
+import java.util.Iterator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.text.NumberFormat;
+import java.util.Calendar;
+
+/**
+  * This class creates the statistics panel on the Admin window. The statistics panel can display reports and graphs of data, revenue, registered person,
+  * sold cards, and validations.
+  * @author Erik Haider Forsén
+  * @author Ole Hansen
+  * @author Julie Hill Roa
+  * @version 0.9
+  */
+
+
+public class AdminStatisticsPanel extends JPanel
+{	
+	private JLabel from, to;
+	private JTextField fromFld, toFld;
+	private JTextArea display;
+	private JPanel displayPnl, graphPnl;
+	private JButton soldCardsBtn, regPersBtn, revenueBtn, liftPassBtn;
+	private JTabbedPane tabDisp; 
+	private JScrollPane scroll;
+	private Listener listener;
+	private Personlist list;
+	private Calculator cal;
+	private List<Validations> validations;
+	private Cardlist cardregistry;
+	private Toolkit toolbox;
+	private NumberFormat paymentFormat; 
+
+	private SimpleDateFormat formatter;
+
+	public static String scale; 
+	public final static int CARDS = 1;
+	public final static int VALIDS = 2;
+	public final static int PERSON = 3; 
+	public final static int REVENUE = 4; 
+
+	private int[][] graph;
+
+/**
+  * This constructor creates elements and place them on the panel. It also receives the necessary data to create the reports and graphs.
+  * @param l 	The personlist to show stats about
+  * @param v 	The validations to show stats about
+  * @param cr 	The cardlist to show stats about
+  */
+	public AdminStatisticsPanel(Personlist l,List<Validations> v, Cardlist cr )
+	{
+		setBackground(new Color(200, 230, 255));
+
+
+		// setting "firstLight" to the day when our Skiresort first saw the day of light, currently 010113.
+
+		formatter = new SimpleDateFormat("ddMMyy"); 
+		paymentFormat = NumberFormat.getCurrencyInstance( new Locale( "no","NO" ) );
+		
+
+		scale = "Dager";
+		list = l;
+		validations = v;
+		cardregistry = cr;
+		toolbox = Toolkit.getDefaultToolkit();
+
+
+		cal = new Calculator(list, validations, cardregistry);
+
+		listener = new Listener();
+		
+		setLayout( new GridBagLayout() );
+	
+		displayPnl = new JPanel();
+		displayPnl.setLayout(new BorderLayout());
+		
+		tabDisp = new JTabbedPane();
+
+		graph = new int[1][10];
+
+		graphPnl = new GraphPanel( graph, "x", "y", "", 0 ); 
+		displayPnl.setBackground(new Color(200, 230, 255));
+		tabDisp.addTab("Rapport", displayPnl);
+		tabDisp.addTab("Grafisk visning", graphPnl);
+
+		tabDisp.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		
+
+
+		soldCardsBtn = new JButton( "Solgte kort" );
+		soldCardsBtn.setToolTipText("Se hvor mange kort som ble solg over en periode");
+		soldCardsBtn.addActionListener( listener );
+		
+
+		regPersBtn = new JButton( "Registrerte personer" );
+		regPersBtn.setToolTipText("Se hvor mange personer som ble registrert over en periode");
+		regPersBtn.addActionListener( listener );
+		
+
+		revenueBtn = new JButton( "Omsetning" );
+		revenueBtn.setToolTipText("Se hvor stor omsetning sentret hadde over en periode");
+		revenueBtn.addActionListener( listener );
+		
+
+		liftPassBtn = new JButton( "Passeringer i heis" );
+		liftPassBtn.setToolTipText("Se hvor mange heis passeringer det var over en periode (skriv inn heisnr for å bare trekke ut fra en heis)");
+		liftPassBtn.addActionListener( listener );
+	
+
+		from = new JLabel( "Fra: " );
+		fromFld = new JTextField(4);
+		fromFld.setToolTipText("Sett inn en startdato du ønsker og beregne info fra");
+		fromFld.setEditable( true );
+	
+
+		to = new JLabel( "Til: " );
+		toFld = new JTextField(4);
+		toFld.setToolTipText("Sett inn en sluttdato du ønsker og beregne info til");
+		toFld.setEditable( true );
+
+	
+
+		display = new JTextArea(1,65);
+		display.setEditable( false );
+
+		scroll = new JScrollPane(display);
+		displayPnl.add(scroll);
+		displayPnl.add(display, BorderLayout.CENTER);
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		c.anchor = GridBagConstraints.LINE_START;
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(0,0,0,0);
+//FØRSTE - KOLONNE /////////////////////////////////////////
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 0;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(soldCardsBtn, c);
+		
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 1;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(regPersBtn, c);
+
+		c.gridheight = 1;
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 2; 
+		c.gridwidth = 1;
+		c.weighty = 0.2;
+		add(revenueBtn, c);
+
+		c.gridheight = 1;
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 3; 
+		c.gridwidth = 1;
+		c.weighty = 0.2;
+		add(liftPassBtn, c);
+
+
+//ANDRE-KOLONNE //////////////////////////////////////////////
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 1; 
+		c.gridy = 0;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(from, c);
+
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 1; 
+		c.gridy = 1;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(to,c);	
+
+	
+
+//tredje kolonne /////////////////////////////
+
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 2; 
+		c.gridy = 0;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(fromFld, c);
+
+		c.gridheight = 1; 
+		c.weightx = 0.5;
+		c.gridx = 2; 
+		c.gridy = 1;
+		c.gridwidth = 1; 
+		c.weighty = 0.2;
+		add(toFld,c);	
+
+
+		c.gridheight = 1;
+		c.weightx = 0.5;
+		c.gridx = 0; 
+		c.gridy = 10; 
+		c.gridwidth = 3;
+		c.weighty = 0.2;
+		add(tabDisp, c);
+
+
+
+	}
+
+/**
+  * This method will gather the data based on a time range, compile it to a nested array, and put the data to display as a report and as a graph. 
+  */
+	private void totalRegPepole()
+	{
+
+		Date start;
+		Date end; 
+		
+		start = getStartDate();
+		end = getEndDate();	
+
+		if( start == null || end == null )
+			return;
+
+		int[][] regPeopleIntrvl = cal.totalRegPeople(start, end);
+		
+		// the following code is necessary to repaint the graph with new data. 		
+		graphPnl = new GraphPanel( regPeopleIntrvl, scale, "Ant", formatter.format(start) + " - " + formatter.format(end), PERSON );
+		int idx = tabDisp.getSelectedIndex();
+		tabDisp.remove( 1 );
+		tabDisp.add("Grafisk visning", graphPnl);
+		tabDisp.setSelectedIndex( idx );
+
+		display.setText("Registrerte personer i intervallet " + formatter.format(start) + " til " + formatter.format(end) +":\n");
+		int total = 0; 
+		for( int i = 0; i < regPeopleIntrvl.length; i++ )
+		{
+			for( int j = 0; j < regPeopleIntrvl[i].length; j++ )
+				total += regPeopleIntrvl[i][j];
+		}
+		display.append( "" + total );
+		
+	}
+/**
+  * This method will gather the data based on a time range, compile it to a nested array, and put the data to display as a report and as a graph. 
+  */
+	private void totalSoldCard()
+	{
+		Date start;
+		Date end; 
+		
+		start = getStartDate();
+		end = getEndDate(); 
+
+		if( start == null || end == null )
+			return;
+
+		int[][] soldCardIntrvl = cal.totalSoldCard(start, end);
+
+		// the following code is necessary to repaint the graph with new data. 
+		graphPnl = new GraphPanel( soldCardIntrvl, scale, "Ant", formatter.format(start) + " - " + formatter.format( end ), CARDS );
+		int idx = tabDisp.getSelectedIndex();
+		tabDisp.remove( 1 );
+		tabDisp.add( "Grafisk visning", graphPnl );
+		tabDisp.setSelectedIndex( idx );
+		
+		int[] total = new int[5];
+		
+		for( int i = 0; i < soldCardIntrvl.length; i++ )
+		{
+			for( int j = 0; j < soldCardIntrvl[i].length; j++ )
+				total[i] += soldCardIntrvl[i][j];
+		}
+		display.setText("Solgte skikort i intervallet " + formatter.format(start) + " til " + formatter.format(end) + ":");
+		display.append( "\nDagskort: \t" + total[Skicard.DAYCARD] );
+		display.append( "\nTimeskort: \t" + total[Skicard.HOURCARD] );
+		display.append( "\nSesongkort: \t" + total[Skicard.SEASONCARD] );
+		display.append( "\nKlippekort: \t" + total[Skicard.PUNCHCARD] );
+		display.append( "\nFysiske kort: \t" + total[4] );
+	}
+
+/**
+  * This method will gather the data based on a time range, compile it to a nested array, and put the data to display as a report and as a graph. 
+  */
+	private void passings()
+	{
+		Date start; 
+		Date end; 
+
+		start = getStartDate(); 
+		end = getEndDate(); 
+
+		if( start == null || end == null )
+			return; 
+
+		int[][] passingsIntrvl = cal.showPassings( start, end );
+		
+		// the following code is necessary to repaint the graph with new data. 
+		graphPnl = new GraphPanel( passingsIntrvl, scale, "Ant", formatter.format( start ) + " - " + formatter.format( end ), VALIDS ); 
+		int idx = tabDisp.getSelectedIndex();
+		tabDisp.remove( 1 );
+		tabDisp.add( "Grafisk visning", graphPnl );
+		tabDisp.setSelectedIndex( idx );
+
+		int[] passings = new int[Info.LIFTS];
+		int total = 0; 
+		display.setText("Heispasseringer i intervallet " + formatter.format(start) + " til " + formatter.format(end) + ":\n\n");
+		
+		for( int i = 0; i < passingsIntrvl.length; i++ )
+		{
+			display.append("Heis nr: " + (i+1) + "\t\t" );
+			for( int j = 0; j < passingsIntrvl[i].length; j++ )
+			{
+				passings[i] += passingsIntrvl[i][j];
+				total += passingsIntrvl[i][j];
+			}
+			display.append( passings[i] + "\n" );
+		}
+
+		display.append("\nTotalt: " + "\t\t" + total ); 
+	}
+
+/**
+  * This method will gather the data based on a time range, compile it to a nested array, and put the data to display as a report and as a graph. 
+  */
+	private void revenue()
+	{
+
+		Date start;
+		Date end; 
+		
+		start = getStartDate();
+		end = getEndDate();	
+
+		if( start == null || end == null )
+			return;
+
+		int[][] totalRevenue = cal.totalRevenue(start, end);
+		
+		// the following code is necessary to repaint the graph with new data. 		
+		graphPnl = new GraphPanel( totalRevenue, scale, "KR", formatter.format(start) + " - " + formatter.format(end), REVENUE );
+		int idx = tabDisp.getSelectedIndex();
+		tabDisp.remove( 1 );
+		tabDisp.add("Grafisk visning", graphPnl);
+		tabDisp.setSelectedIndex( idx );
+
+		display.setText("Omsetning i intervallet " + formatter.format(start) + " til " + formatter.format(end) +":\n");
+	
+		int[] total = new int[5];
+		
+		int sum = 0;
+
+		for( int i = 0; i < totalRevenue.length; i++ )
+		{
+			for( int j = 0; j < totalRevenue[i].length; j++ )
+			{
+				total[i] += totalRevenue[i][j];
+				sum += totalRevenue[i][j];
+			}
+		}
+		display.append( "\nDagskort: \t" + paymentFormat.format( total[Skicard.DAYCARD] ) );
+		display.append( "\nTimeskort: \t" + paymentFormat.format( total[Skicard.HOURCARD] ) );
+		display.append( "\nSesongkort: \t" + paymentFormat.format( total[Skicard.SEASONCARD] ) );
+		display.append( "\nKlippekort: \t" + paymentFormat.format( total[Skicard.PUNCHCARD] ) );
+		display.append( "\nFysiske kort: \t" + paymentFormat.format( total[4] ) );
+		display.append( "\n\nTotalt: \t" + paymentFormat.format( sum ) );
+		
+	}
+
+
+
+
+/**
+  * A method to retrieve the start Date of the range. If no start Date is specified, it will use the date when this Skiresort first opened.
+  * @return Returns a Date object for the beginning of the range
+  * @see Info#firstLight
+  */
+	private Date getStartDate()
+	{
+		Date start; 
+		try
+		{
+			start = formatter.parse(fromFld.getText());
+		}
+		catch( ParseException pe )
+		{
+			if( !fromFld.getText().isEmpty() )
+			{
+				JOptionPane.showMessageDialog(null, "Datoen må være på formen ddmmyy!");
+				return null;
+			}
+
+			start = Info.firstLight;
+			fromFld.setText( formatter.format( start ) );
+		}
+
+		return start;
+
+	}
+
+/**
+  * A method to retrieve the end Date of the range. If no end Date is specified, it will use the today's date.
+  * @return Returns a Date object for the end of the range
+  */
+	private Date getEndDate()
+	{
+		Calendar cal = Calendar.getInstance();
+		Date end; 
+		try
+		{
+			end = formatter.parse(toFld.getText());
+			cal.setTime( end );
+			cal.set( Calendar.HOUR_OF_DAY, 23 );
+			cal.set( Calendar.MINUTE, 59 );
+			end = cal.getTime();
+
+		}
+		catch( ParseException pe )
+		{
+			if( !toFld.getText().isEmpty() )
+			{
+				JOptionPane.showMessageDialog(null, "Datoen må være på formen ddmmyy!");
+				return null;
+			}
+			end = new Date(); 
+			toFld.setText( formatter.format( end ) );
+		}
+
+		return end;
+	}
+
+
+	private class Listener implements ActionListener
+  	{
+   		public void actionPerformed( ActionEvent e )
+    	{ 	
+
+
+       		if (e.getSource() == soldCardsBtn )
+       		{
+       			totalSoldCard();
+       		}
+       		else if (e.getSource() == regPersBtn )
+       			totalRegPepole();
+       		else if (e.getSource() == liftPassBtn)
+       			 passings();
+       		else if (e.getSource() == revenueBtn)
+       			revenue();
+    		
+
+    	}
+	}
+
+
+
+}
+	
---
+++ Skiresort.java
@@ -0,0 +1,212 @@
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Objects;  
+
+public class Skiresort
+{
+	private static String message = null; 
+	private static Personlist registry; 
+	private static Cardlist cardregistry;
+	private static List<Validations> validations;
+	private static RandomData r;
+
+	public static Salesclerk s;
+	public static Info i;
+	public static Admin a;
+	public static Control lift1;
+	public static Control lift2; 
+
+	public static Lift chLift;
+	public static Lift tLift; 
+
+	public static boolean unsaved; 
+
+	public static void main(String[] args)
+	{
+		//System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MyApplication");
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		Toolkit verktoykasse = Toolkit.getDefaultToolkit();
+  		String bildefil = "img/offpist_logo.png";
+  		final Image ikon = verktoykasse.getImage(bildefil);
+		
+		unsaved = false; 
+
+		Skiresort test = new Skiresort(); 
+
+		test.readFile(); 
+		
+		chLift = new Chairlift(validations, registry, "Eventyrheisen", 2000, 3 );
+
+		tLift = new Tcuplift( validations, registry, "Lårgnagern", 1433 );
+		
+
+
+		EventQueue.invokeLater( new Runnable()
+		{
+			public void run()
+			{
+				// Fjern denne etterhvert
+				int yn = JOptionPane.showOptionDialog(null, "Vil du få litt tilfeldig data?", "Random stash", JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, null, null );
+
+				if( yn == JOptionPane.YES_OPTION )
+					r = new RandomData( registry, cardregistry, validations );
+
+				s = new Salesclerk(registry, cardregistry, message); 
+				s.setJMenuBar( new MenuBar().createMenu() );
+				s.setVisible( true );
+				s.setIconImage(ikon);
+				s.setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE );
+				s.addWindowListener( new WindowAdapter() 
+				{
+					public void windowClosing( WindowEvent e )
+					{
+						if( checkForUnsaved())  
+							System.exit( 0 );
+					}
+				});
+
+				/*final Drittvindu  vindu = new Drittvindu( validations, registry, cardregistry);
+				vindu.setIconImage(ikon);
+    			vindu.setJMenuBar(new MenuBar().createMenu() );
+				vindu.setLocation(700,0); 		
+*/
+
+				i = new Info();
+				i.setJMenuBar( new MenuBar().createMenu() );
+				i.setVisible(false);
+  		
+
+
+/*
+				final Control control = new Control();
+    			control.setLocation(0, 500);
+*/				
+
+				a = new Admin( registry,validations, cardregistry);
+				a.setLocation(0,425);
+				a.setJMenuBar( new MenuBar().createMenu() );
+				a.setIconImage(ikon);    
+				a.setVisible( false );
+
+				
+				lift1 = new Control( registry,  chLift, cardregistry );
+				lift1.setJMenuBar( new MenuBar().createMenu() );
+				lift1.setLocation(700,625);
+				lift1.setIconImage(ikon);
+				lift1.setVisible(false);
+
+				
+
+				lift2 = new Control( registry, tLift, cardregistry );   
+				lift2.setJMenuBar( new MenuBar().createMenu() );
+				lift2.setLocation(200,625);
+				lift2.setIconImage(ikon);
+				lift2.setVisible(false);
+			}			
+
+		} );
+	}
+
+	private static void readFile()
+	{
+		try( ObjectInputStream input = new ObjectInputStream(
+			new FileInputStream( "data.dta" ) ) )
+		{
+			registry = (Personlist) input.readObject();
+			validations = (List<Validations>) input.readObject();
+			cardregistry = (Cardlist) input.readObject();
+			Person.setNext( input.readInt() );
+			Card.setNext( input.readInt() );
+		}
+		
+		catch( ClassNotFoundException cnfe )
+		{
+			message = "Ingen personliste funnet, oppretter ny";
+			registry = new Personlist();
+			validations = new LinkedList<>();
+			cardregistry = new Cardlist();
+		}
+		catch( FileNotFoundException fnfe )
+		{
+			message = "Finner ikke datafilen, oppretter ny datafil";
+			registry = new Personlist();
+			validations = new LinkedList<>();
+			cardregistry = new Cardlist();
+		}
+		catch( IOException ioe )
+		{
+			message = "Feil med lesing fra fil, oppretter ny datafil";
+			registry = new Personlist();
+			validations = new LinkedList<>();
+			cardregistry = new Cardlist();
+		}
+	}
+
+	public static void saveFile()
+	{
+		try( ObjectOutputStream output = new ObjectOutputStream(
+			new FileOutputStream( "data.dta" ) ) )
+		{
+			output.writeObject( registry );
+			output.writeObject( validations );
+			output.writeObject(cardregistry);
+			output.writeInt( Person.readNext() );
+			output.writeInt( Card.readNext() );
+			
+			s.statusTxt.setText( "alt ble lagret" );
+			unsaved = false; 
+
+		}
+		catch( NotSerializableException nse )
+		{
+			JOptionPane.showMessageDialog(null, "Objektet er ikke serialisert", "Feil!", JOptionPane.ERROR_MESSAGE );
+			nse.printStackTrace( System.out );
+		}
+		catch( IOException ioe )
+		{
+			JOptionPane.showMessageDialog(null, "Feil ved skriving til fil", "Feil!", JOptionPane.ERROR_MESSAGE );
+		}
+	}
+
+	public static boolean checkForUnsaved()
+	{
+		if( !unsaved )
+			return true;	
+
+		String[] options = {"Lagre", "Ikke lagre", "Avbryt"};
+
+		int n = JOptionPane.showOptionDialog(null, "Du har ulagrete endringer.", "Lagringsdialog", JOptionPane.YES_NO_CANCEL_OPTION,
+		JOptionPane.QUESTION_MESSAGE, null, options, options[0] );
+
+
+		switch( n )
+		{
+			case JOptionPane.YES_OPTION:
+				saveFile();
+				return true;
+			case JOptionPane.NO_OPTION:
+				return true; 
+			case JOptionPane.CANCEL_OPTION:
+				return false; 
+		}
+
+		return false; 
+	}
+
+/*	<datafelter>	
+
+	<konstruktør som oppretter alle lister / vinduer>
+
+	<usikkert hva annet som skal være av metoder her>
+
+	<metode for å lese fra fil>
+	<main metoden>
+
+	<vinduslytter for å lagre til fil>*/
+
+}
