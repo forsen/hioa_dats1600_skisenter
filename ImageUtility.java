f098d8762ac4e5903e93a8b3359aadbbcf3ede42
Julie Hill Roa <julie.hill.roa@hotmail.com>
2013-05-13 12:47:31 +0200

    commentert

--- ImageUtility.java
+++ ImageUtility.java
@@ -8,12 +8,27 @@ import java.awt.Graphics2D;
 import javax.imageio.ImageIO;
 import java.nio.channels.FileChannel;
 
-
+/**
+  * This class is a helping class for copying, moving and saving an personal image.    
+  * 
+  * @author Erik Haider Forsén
+  * @author Ole Hansen
+  * @author Julie Hill Roa
+  * @version 0.9
+  */
 
 public class ImageUtility
 {	
 	
 	File orgfile;
+
+/** 
+  *This methode draws a buffred image like the original image, but with a different size and with JPG as last name.
+  * 
+  * @param f: a File object for the image you want to resize
+  * @return Returns a file object for the new image
+  * @see CustWindowPanel
+  */
 
 	private File drawImage( File f)
 	{	
@@ -21,25 +36,27 @@ public class ImageUtility
 		orgfile = f;
 		try
 		{
-			BufferedImage img = ImageIO.read(f);
+			BufferedImage img = ImageIO.read(f); //Draws the file f
 
 		
 		
 			int width = 74;
 			int height = 120;
 
-			BufferedImage resizedPic = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+			
+			BufferedImage resizedPic = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); 
+			//Draws an empty image with the specific size
 			Graphics2D gr = resizedPic.createGraphics();
 			gr.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
 			gr.setColor(Color.WHITE);
 			gr.fillRect(0, 0, width, height);
 			gr.setComposite(AlphaComposite.SrcOver);
-			gr.drawImage(img, 0, 0, width, height, null);
+			gr.drawImage(img, 0, 0, width, height, null); //Puts the drws file onto to the Graphics object wth the new size
 			gr.dispose();
 		
-		
+			//Converts it to a File and let it have the same name as the old image.
 			File nfile = new File(orgfile.getName());
-			ImageIO.write(resizedPic, "png", nfile);
+			ImageIO.write(resizedPic, "jpg", nfile);
 			return nfile;
 		}
 		catch(IOException ie)
@@ -54,23 +71,32 @@ public class ImageUtility
 	}
 
 	
-
+/** 
+  *This methode makes a new file with the name of the persons custId, and puts the image drawn by the methode drawImage() 
+  * onto the new file.
+  *
+  * @param f: File object for the chosen personal image. 
+  * @param p: Witch person the image belongs to. 
+  * @see CustWindowPanel
+  * @see drawImage( File f)
+  */
 	public void saveImage(File f, Person p)
 	{
 		
 
 		try
 		{
+			//Creates a new file with the name of the persons custId
 			File persPic = new File("persImg/" + p.getCustId()+".jpg");
   			if (!persPic.exists()) 
   			{
        			persPic.createNewFile();
   			}
   			
+  			//Gets the file (image) back resized
   			File nf = drawImage(f);
   			
-
-
+  			//Opens the connection to the file so it can be be moved and renamed
   			FileChannel fsource = new FileInputStream(nf).getChannel();
    			FileChannel target = new FileOutputStream(persPic).getChannel();
    			target.transferFrom(fsource, 0, fsource.size());
