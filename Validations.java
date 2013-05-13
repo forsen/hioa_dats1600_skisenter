564a4e8b552686033ddaedff3ba2442afac2b2ea
Ole Hansen <olehansen92@gmail.com>
2013-05-13 09:23:04 +0200

    javadøkk

--- Validations.java
+++ Validations.java
@@ -2,11 +2,32 @@ import java.util.Date;
 import java.util.Calendar;
 import java.io.Serializable;
 
+/**
+ * Validations creates an object of a validation.
+ * 
+ * @author Erik Haider Forsén
+ * @author Ole Hansen
+ * @author Julie Hill Roa
+ * @version 0.9
+ */
+
 public class Validations implements Serializable
 {
+
 	private int liftId;
 	private Card card;
 	private Date date; 
+
+
+/**
+  *	Creates the object Validations
+  * It contains the liftnumber, a Card object, and a Date object.
+  *
+  *	@param l 		the lift ID
+  * @param c 		the card object
+  * @param d 		the date object 
+  * @see Skisenter
+  */
 
 	public Validations( int l, Card c, Date d )
 	{
@@ -20,20 +41,47 @@ public class Validations implements Serializable
 		Skiresort.unsaved = true; 
 	}
 
+/**
+  * Returns the lift ID
+  *
+  * @return 	returns lift ID
+  */
+
 	public int getLiftId()
 	{
 		return liftId;
 	}
+
+/**
+  * Returns the Card object
+  *
+  * @return 	returns object Card
+  */
 
 	public Card getCard()
 	{
 		return card;
 	}
 
+/**
+  * Returns the Date object
+  *
+  * @return 	returns object Date
+  */
+
+
 	public Date getDate()
 	{
 		return date;
 	}
+
+
+/**
+  * This is Validations toString. It returns a string that contains liftID, object Cards toString, 
+  * card type, and the time of a validation.
+  *
+  * @return 	returns object Date
+  */
 
 	public String toString()
 	{
