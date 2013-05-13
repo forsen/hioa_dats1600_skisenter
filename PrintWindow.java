a242b757e522c893bec96e58969bc9990f51a8a9
Erik Haider Forsen <erik@forsen.no>
2013-05-13 12:14:57 +0200

    javadoc

--- PrintWindow.java
+++ PrintWindow.java
@@ -2,6 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+  * This class sets up a JDialog window to put the painted
+  * Skicard or Receipt on.
+  * @author Erik Haider Forsén
+  * @author Ole Hansen
+  * @author Julie Hill Roa
+  * @version 0.9
+  */
+
 public class PrintWindow extends JDialog
 {
 
@@ -10,6 +19,10 @@ public class PrintWindow extends JDialog
 	private Listener btnListener;
 	private JScrollPane scroll;
 
+/**
+  * This is the default constructor. It will set up the elements and layout.
+  * @param parent This is the parent window this JDialog belongs to.
+  */
 	public PrintWindow( Window parent )
 	{
 		super( parent, "Printvindu" );
@@ -24,7 +37,19 @@ public class PrintWindow extends JDialog
 
 		setVisible( true ); 
 	}
-
+/**
+  * This is the constructor to set up the window for the receipt. 
+  * It will receive the necessary data, and create a ReceiptPainting object
+  * and put it on screen.
+  * @param parent 	The parent window this JDialog belongs to
+  * @param j 	The textarea where all the items to be printed on the receipt is
+  * @param o 	A double array containing the payments made. Index tells if it's paid
+  * 			with card or cash, value tells how much is paid.
+  * @param s 	A double containing the sum of the order. 
+  * @see CashRegister#CASH
+  * @see CashRegister#CARD
+  * @see ReceiptPainting
+  */
 	public PrintWindow( Window parent, JTextArea j, double[] o, double s )
 	{
 		this( parent ); 
@@ -38,6 +63,14 @@ public class PrintWindow extends JDialog
 
 	}
 
+/**
+  * This is the constructor to set up the window for the card.
+  * It will receive the necessary data, and create a CardPainting object
+  * and put it on screen.
+  * @param parent 	The parent window this JDialog belongs to
+  * @param c 		The card to be printed
+  * @see CardPainting
+  */
 	public PrintWindow( Window parent, Card c )
 	{
 		this( parent ); 
